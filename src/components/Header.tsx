5a80c03a707b01e2217a138459efd282e8ca7f82
Viktor Teodosin <teodosin@protonmail.com>
2025-01-10 10:19:17 +0200

    Button formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,17 @@ import Button from "./Button";
 export default function Header() {
   const { theme, toggleTheme } = useTheme();
 
+  let label = theme === "dark" ? "light" : "dark";
+
   return (
     <header className="flex items-center justify-between p-4 top-0 sticky">
-      <Button onClick={() => {
-        console.log("Changing theme to: ", theme);
-        toggleTheme();
-      }
-      } label="dark mode" />
+      <Button
+        onClick={() => {
+          console.log("Changing theme to: ", theme);
+          toggleTheme();
+        }
+        }
+        label={label} />
     </header>
   )
 }
