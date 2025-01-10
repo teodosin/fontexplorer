05a2ef9ed873c5b834a518ee1ca2606d9df1b3ca
Viktor Teodosin <teodosin@protonmail.com>
2025-01-10 20:11:55 +0200

    Dynamic imports in clientproviders

--- src/ClientProviders.tsx
+++ src/ClientProviders.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import React from 'react';
-import { ThemeProvider } from '@/ThemeContext';
-import FontSaver from '@/components/FontSaver';
-import Header from '@/components/Header';
+import dynamic from 'next/dynamic';
+
+const ThemeProvider = dynamic(() => import('@/ThemeContext').then(mod => mod.ThemeProvider), { ssr: false });
+const FontSaver = dynamic(() => import('@/components/FontSaver'), { ssr: false });
+const Header = dynamic(() => import('@/components/Header'), { ssr: false });
 
 interface ClientProvidersProps {
   children: React.ReactNode;
