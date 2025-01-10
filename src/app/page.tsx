d7ff258d178e27cfc9c6dc4cfd12339f1460f721
Viktor Teodosin <teodosin@protonmail.com>
2025-01-10 11:42:20 +0200

    Style slider component, implement localstorage for current state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CurrentFont from "@/components/CurrentFont";
 import FontBlock from "@/components/FontBlock";
 import Slider from "@/components/Slider";
 import TextInput from "@/components/TextInput";
-import { loadCurrents } from "@/fonts";
+import { loadCurrents, saveCurrents } from "@/fonts";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -12,24 +12,40 @@ export default function Home() {
   const [ previewText, setPreviewText ] = useState("");
   const [ previewSize, setPreviewSize ] = useState(16);
 
+  const [ relations, setRelations ] = useState([]);
+  const [ suggestions, setSuggestions ] = useState([]);
+
   useEffect(() => {
     let data = loadCurrents();
-    let font = data.currentFont;
-    console.log("Setting the current font to " + font);
-    setCurrentFont(font);
+    setCurrentFont(data.currentFont);
+    setPreviewText(data.currentPreviewText);
+    setPreviewSize(data.currentFontSize);
   }, []);
+
+  useEffect(() => {
+    // Saving current state to localstorage
+    saveCurrents({
+      version: "0.0.1",
+      currentFont: currentFont,
+      currentFontSize: previewSize,
+      currentPreviewText: previewText
+    })
+  }, [currentFont, previewSize, previewText]);
 
   return (
     <div className="">
 
       <main className="flex flex-col gap-8 items-center justify-center">
-        <CurrentFont font={currentFont} size={previewSize} text={previewText} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
+        <CurrentFont font={currentFont} size={previewSize} text={previewText} />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <FontBlock font="Georgia" />
         </div>
+
       </main>
 
-      <div className="fixed bottom-0 left-0 right-0 h-20 flex items-center flex-col mb-20">
+      <div className="fixed bottom-0 left-0 right-0 flex items-center flex-col gap-4 mb-20">
           <TextInput onChange={setPreviewText} text={previewText} type="preview-text" />
           <Slider onChange={setPreviewSize} value={previewSize} />
       </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,8 +1,9 @@
+// Todo: Allow label to be changed
 
 export default function Slider({ value, onChange }: { value: number, onChange: (size: number) => void }) {
   return (
     <div className="flex flex-row items-center gap-2">
-        <span>
+        <span className="text-gray-700 dark:text-gray-400">
             Preview Size: {value}
         </span>
 
@@ -12,6 +13,7 @@ export default function Slider({ value, onChange }: { value: number, onChange: (
             max="100"
             value={value}
             onChange={(e) => onChange(parseInt(e.target.value))}
+            className="h-2 bg-gray-400 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
         >
         </input>
     </div>
