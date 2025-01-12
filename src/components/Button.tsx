decb2aedb8feedf1cd0ae761ae04b563b6817b88
Viktor Teodosin <teodosin@protonmail.com>
2025-01-12 20:45:17 +0200

    button and currentfont component adjustment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,11 +9,11 @@ export default function Button({ children, ...props }: ButtonProps) {
     <button
       {...props}
       className={
-        `w-32 rounded-full overflow-hidden bg-white dark:bg-gray-800 dark:text-gray-900 
+        `w-32 rounded-full overflow-hidden bg-white dark:bg-gray-800 
         p-4 justify-center items-center hover:bg-gray-100 
         dark:hover:bg-gray-700 transition-colors pointer-events-auto`
       }>
-      <span className="text-gray-700 text-base">{children}</span>
+      <span className="dark:text-gray-300 text-gray-700 text-base">{children}</span>
     </button>
   )
 }
--- src/components/CurrentFont.tsx
+++ src/components/CurrentFont.tsx
@@ -4,18 +4,33 @@ interface CurrentFontProps {
   font: string
   size: number
   text: string
+  setHistory: (history: string[]) => void
 }
 
-export default function CurrentFont({ font, size, text }: CurrentFontProps) {
+export default function CurrentFont({ font, size, text, setHistory }: CurrentFontProps) {
   let preview = text == '' ? 'Quick Brown Fox' : text
 
   return (
     <div
       className="max-w-36rem w-full px-8 pt-6 pb-16 text-center rounded-full overflow-hidden bg-white dark:bg-gray-800 p-4 justify-center items-center"
     >
-      <h6 className="text-gray-700 dark:text-gray-400 text-base pb-8">
-        {font}
-      </h6>
+      <div className="flex flex-row w-full px-6 justify-between items-center gap-4 pb-8">
+        <button
+          onClick={() => {
+            setHistory((history) => history.slice(0, history.length - 1));
+          }}
+          className="text-gray-700 dark:text-gray-500 hover:text-gray-100 text-base"
+        >
+          back
+        </button>
+
+        <h6 className="text-gray-700 dark:text-gray-400 text-base">
+          {font}
+        </h6>
+
+        <button className="text-white dark:text-gray-800 text-base">forw</button>
+      </div>
+
       <span className="text-gray-900 dark:text-gray-100"
         style={{
           fontFamily: font,
@@ -25,6 +40,7 @@ export default function CurrentFont({ font, size, text }: CurrentFontProps) {
       >
         {preview}
       </span>
+
     </div>
   )
 }
