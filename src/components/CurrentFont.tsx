f800aa6d37929609793be726a014f79d0b3df78c
Viktor Teodosin <teodosin@protonmail.com>
2025-01-12 20:53:15 +0200

    Buttons for going back and forth in history, still needs implementation

--- src/components/CurrentFont.tsx
+++ src/components/CurrentFont.tsx
@@ -4,32 +4,40 @@ interface CurrentFontProps {
   font: string
   size: number
   text: string
-  setHistory: (history: string[]) => void
+  // setHistory: (history: string[]) => void
 }
 
-export default function CurrentFont({ font, size, text, setHistory }: CurrentFontProps) {
+export default function CurrentFont({ font, size, text }: CurrentFontProps) {
   let preview = text == '' ? 'Quick Brown Fox' : text
 
   return (
     <div
-      className="max-w-36rem w-full px-8 pt-6 pb-16 text-center rounded-full overflow-hidden bg-white dark:bg-gray-800 p-4 justify-center items-center"
+      className="max-w-36rem w-full px-8 pt-2 pb-16 text-center rounded-full overflow-hidden bg-white dark:bg-gray-800 p-4 justify-center items-center"
     >
       <div className="flex flex-row w-full px-6 justify-between items-center gap-4 pb-8">
         <button
           onClick={() => {
-            setHistory((history) => history.slice(0, history.length - 1));
+            // logic to go back in history
           }}
-          className="text-gray-700 dark:text-gray-500 hover:text-gray-100 text-base"
+          className={`text-gray-700 dark:text-gray-500 hover:text-gray-100 text-3xl w-12
+            rounded-full hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors`}
         >
-          back
+          ↢
         </button>
 
-        <h6 className="text-gray-700 dark:text-gray-400 text-base">
+        <h6 className="text-gray-700 dark:text-gray-400 text-base pt-2">
           {font}
         </h6>
 
-        <button className="text-white dark:text-gray-800 text-base">forw</button>
-      </div>
+        <button
+          onClick={() => {
+            // logic to go back in history
+          }}
+          className={`text-gray-700 dark:text-gray-500 hover:text-gray-100 text-3xl w-12
+            rounded-full hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors`}
+        >
+          ↣
+        </button>      </div>
 
       <span className="text-gray-900 dark:text-gray-100"
         style={{
