d51714c947613db9568a2828d3493d0eb715bf3f
Viktor Teodosin <teodosin@protonmail.com>
2025-01-12 19:15:27 +0200

    Add web safe fonts to font fetching

--- src/utils/fonts_server.ts
+++ src/utils/fonts_server.ts
@@ -1,5 +1,19 @@
-
 const GOOGLE_FONTS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_FONTS_API_KEY;
+
+// Web safe fonts are not included in the Google Fonts API,
+// but still available in browsers. We add them manually. 
+// https://www.w3schools.com/cssref/css_websafe_fonts.php
+export let webSafeFonts = [
+    {family: "Arial"},
+    {family: "Courier New"},
+    {family: "Georgia"},
+    {family: "Garamond"},
+    {family: "Tahoma"},
+    {family: "Times New Roman"},
+    {family: "Trebuchet MS"},
+    {family: "Verdana"},
+    {family: "Brush Script MT"},
+]
 
 export async function getFontsList(): Promise<any[]> {
     try {
@@ -15,7 +29,9 @@ export async function getFontsList(): Promise<any[]> {
         
         const data = await response.json();
         console.log("Fonts fetched successfully:", data.items.length);
-        return data.items;
+
+        let addedWebSafe = [...data.items, ...webSafeFonts];
+        return addedWebSafe;
     } catch (error) {
         console.error("Error fetching fonts:", error);
         return [];
